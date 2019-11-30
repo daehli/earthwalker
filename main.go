45ef03a8e57a658cb697eb95160d0e599489fb65
Linus <justwitti@gmail.com>
2019-11-30 13:51:43 +0100

    Listening to port

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"flag"
 	"gitlab.com/glatteis/earthwalker/placefinder"
 	"gitlab.com/glatteis/earthwalker/streetviewserver"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
+	port := flag.Int("port", 8080, "the port the server is running on")
+	flag.Parse()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/static/get_places/get_places.html", 301)
 	})
@@ -16,5 +21,5 @@ func main() {
 	http.HandleFunc("/found_points", placefinder.RespondToPoints)
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
 }
