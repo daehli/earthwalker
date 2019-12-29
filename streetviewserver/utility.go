d15ecfb72949f07991c784d1ef4f78c6d665c296
Linus <justwitti@gmail.com>
2019-12-29 17:02:53 +0100

    add / to string regex

--- streetviewserver/utility.go
+++ streetviewserver/utility.go
@@ -5,7 +5,7 @@ import (
 )
 
 // GOAL : MATCH [["Jl. SMA Aek Kota Batu","id"],["Sumatera Utara","de"]]
-var stringRegex = "(\\p{L}| |\\d|\\_|\\-|\\,|\\.)"
+var stringRegex = "(\\p{L}| |\\d|\\_|\\-|\\,|\\.|/)"
 var languageRegex = "\\[\"" + stringRegex + "+\"+,\"" + stringRegex + "{1,10}\"\\]"
 
 var compiledRegexp *regexp.Regexp = regexp.MustCompile(languageRegex)
@@ -13,5 +13,6 @@ var compiledRegexp *regexp.Regexp = regexp.MustCompile(languageRegex)
 // filterStrings filters all string contents from a given string (as byte array),
 // used to strip all localization information from a specific street view packet
 func filterStrings(body []byte) []byte {
-	return []byte(compiledRegexp.ReplaceAllString(string(body), "[\"\",\"\"]"))
+	result := compiledRegexp.ReplaceAllString(string(body), "[\"\",\"\"]")
+	return []byte(result)
 }
--- streetviewserver/utility_test.go
+++ streetviewserver/utility_test.go
@@ -5,9 +5,11 @@ import "testing"
 func TestFilterStrings(t *testing.T) {
 	inputs := []string{
 		"This is a test, and replaced should be this: [[\"Jl. SMA Aek Kota Batu\",\"id\"],[\"Sumatera Utara\",\"de\"]], yes that is what should be replaced.",
+		"[[\"а/д Вятка\",\"ru\"]]",
 	}
 	outputs := []string{
 		"This is a test, and replaced should be this: [[\"\",\"\"],[\"\",\"\"]], yes that is what should be replaced.",
+		"[[\"\",\"\"]]",
 	}
 
 	for i := range inputs {
