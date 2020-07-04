2c40a2323ddc350684c2725b9c4b9cadc9e503c1
jwlarocque <jwlarocque@gmail.com>
2020-07-04 15:24:34 -0600

    Tweak in-game UI

--- frontend-svelte/src/Modify.svelte
+++ frontend-svelte/src/Modify.svelte
@@ -1,6 +1,6 @@
 <script>
     // TODO: this file is getting out of hand
-    
+
     import { onMount } from 'svelte';
 
     let ewapi = new EarthwalkerAPI();
@@ -20,6 +20,8 @@
         [300, 225],
         [500, 375],
         [800, 600],
+        [1200, 900],
+        [1600, 1200],
     ];
     let curMapSize = 1;
     // sets title to "earthwalker"
@@ -264,6 +266,8 @@
         opacity: 50%;
         width: 300px;
         height: 225px;
+        max-height: calc(100vh - 300px);
+        max-width: calc(100vw - 60px);
         align-self: flex-end;
         margin-left: auto;
         margin-bottom: 0.5rem;
@@ -318,16 +322,25 @@
     }
 
     #top {
+        flex: 0 0;
         user-select: none;
-        width: 300px;
+        max-width: 300px;
         margin-left: auto;
-        text-align: right;
+        text-align: center;
         float: right;
+        color: white;
+        background-color: rgba(0, 0, 0, 0.5);
+        border-radius: 0.25rem;
+        padding: 0.5rem;
+    }
+
+    #top button {
+        margin: 0.25rem;
     }
 
     #middle {
         position: relative;
-        flex: 1 1;
+        flex: 10 1;
         width: 300px;
         margin-top: .5rem;
         margin-bottom: .5rem;
@@ -338,6 +351,7 @@
         float: right;
         clear: both;
         background-color: white;
+        overflow: auto;
     }
 
     #middle h3 {
@@ -355,7 +369,7 @@
     }
 
     #bottom {
-        flex: 0 0;
+        flex: 1 1;
         float: right;
         clear: both;
     }
@@ -369,32 +383,55 @@
 
 <div id="right-bar">
     <div id="top">
-        <span class="round-info-span align-middle">
-            {"Round: " + (challengeResult && map ? (challengeResult.Guesses.length + 1) + "/" + map.NumRounds : "loading...")}
-            <br/>
-            {"Total points: " + totalScore}
-            <br/>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    Round
+                </div>
+                <div class="col">
+                    {challengeResult && map ? (challengeResult.Guesses.length + 1) + " of " + map.NumRounds : "loading..."}
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-nowrap">
+                    Total Points
+                </div>
+                <div class="col">
+                    {totalScore}
+                </div>
+            </div>
             {#if timeRemaining > 0}
-                Time: {Math.floor(timeRemaining / 60).toString()}:{Math.floor(timeRemaining % 60).toString().padStart(2, '0')}
-                <br/>
+                <div class="row">
+                    <div class="col">
+                        Time
+                    </div>
+                    <div class="col">
+                        {Math.floor(timeRemaining / 60).toString()}:{Math.floor(timeRemaining % 60).toString().padStart(2, '0')}
+                    </div>
+                </div>
             {/if}
-        </span>
-        <button 
-            class="btn btn-light mx-sm-2 align-middle"
-            on:click={() => {
-                if (marker != null) {
-                    // Put marker into session storage
-                    console.log(marker.getLatLng());
-                    sessionStorage.setItem("lastMarker", JSON.stringify({
-                        "lat": marker.getLatLng().lat,
-                        "lng": marker.getLatLng().lng,
-                        "roundNumber": challengeResult.Guesses.length,
-                        "gameID": challengeResultID,
-                    }));
-                }
-                // https://www.phpied.com/files/location-location/location-location.html
-                location = location;
-            }}>go to start</button>
+            <div class="row justify-content-center">
+                <button 
+                    class="btn btn-light btn-sm"
+                    on:click={() => {
+                        if (marker != null) {
+                            // Put marker into session storage
+                            console.log(marker.getLatLng());
+                            sessionStorage.setItem("lastMarker", JSON.stringify({
+                                "lat": marker.getLatLng().lat,
+                                "lng": marker.getLatLng().lng,
+                                "roundNumber": challengeResult.Guesses.length,
+                                "gameID": challengeResultID,
+                            }));
+                        }
+                        // https://www.phpied.com/files/location-location/location-location.html
+                        location = location;
+                    }}
+                >
+                    return to start
+                </button>
+            </div>
+        </div>
     </div>
     <div id="middle" class={showSettings ? "" : "hidden"}>
         <h3>Settings</h3>
@@ -430,8 +467,8 @@
                 </button>
             </div>
             <div id="navigation-bar" class="btn-group btn-group-sm float-right">
-                <button class="btn btn-light" on:click={() => {scaleMap(true)}}>⬉</button>
-                <button class="btn btn-light" on:click={() => {scaleMap(false)}}>⬊</button>
+                <button class="btn btn-light" on:click={() => {scaleMap(true)}} disabled={curMapSize == mapSizes.length - 1}>⬉</button>
+                <button class="btn btn-light" on:click={() => {scaleMap(false)}} disabled={curMapSize == 0}>⬊</button>
             </div>
             <button 
                 bind:this={guessButton}
