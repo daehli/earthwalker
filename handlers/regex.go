04909ca79d161ea15d7dcf745fb58e6307c96017
Linus <justwitti@gmail.com>
2020-08-01 12:01:11 +0200

    remove shop icons

--- handlers/regex.go
+++ handlers/regex.go
@@ -1,18 +1,28 @@
 package handlers
 
 import (
+	"fmt"
 	"regexp"
 )
 
-// GOAL : MATCH [["Jl. SMA Aek Kota Batu","id"],["Sumatera Utara","de"]]
-var stringRegex = "(\\p{L}| |\\d|\\_|\\-|\\,|\\.|/)"
-var languageRegex = "\\[\"" + stringRegex + "+\"+,\"" + stringRegex + "{1,10}\"\\]"
+// Eliminate street names from packets
+// match [["Jl. SMA Aek Kota Batu","id"],["Sumatera Utara","de"]] (see regex_test.go)
+var streetNameChars = "(\\p{L}| |\\d|\\_|\\-|\\,|\\.|/)"
+var listOfStreetNames = "\\[\"" + streetNameChars + "+\"+,\"" + streetNameChars + "{1,10}\"\\]"
+var streetNameRegex *regexp.Regexp = regexp.MustCompile(listOfStreetNames)
 
-var compiledRegexp *regexp.Regexp = regexp.MustCompile(languageRegex)
+// Eliminate shop icons from packets. These are an object in json. for an example, see regex_test.go.
+// Heuristic used: Always starts with [[[ and ends with ]]], not followed by a [\".
+// Always contains https://maps.gstatic.com/mapfiles/annotations/icons/ somewhere.
+// var shopChars = "(\\p{L}| |\\d|\\_|\\-|\\,|\\.|\\[|\\])"
+// var shopIcons = "[[[" + shopChars + "*?" + "https:\\/\\/maps.gstatic.com\\/mapfiles\\/annotations\\/icons\\/" + shopChars + "*?" + "]]],[^\\[\"]"
+var shopIcons = "\\[\\[\\[([\\[\\],\":\\/. \\\\_\\-\\pL\\pN\\pM\\pZ\\pP\\pC])*https:\\/\\/maps\\.gstatic\\.com\\/mapfiles\\/annotations\\/icons\\/([0-9\\[\\],\":\\/. \\\\_\\-\\pL])*\\]\\]\\],"
+var shopIconRegex *regexp.Regexp = regexp.MustCompile(shopIcons)
 
 // filterStrings filters all string contents from a given string (as byte array),
 // used to strip all localization information from a specific street view packet
 func filterStrings(body []byte) []byte {
-	result := compiledRegexp.ReplaceAllString(string(body), "[\"\",\"\"]")
+	result := streetNameRegex.ReplaceAllString(string(body), "[\"\",\"\"]")
+	result = shopIconRegex.ReplaceAllString(result, "[],")
 	return []byte(result)
 }
