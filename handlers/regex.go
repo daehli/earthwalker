37428f02ebde2b186f20a9bfca6b4efa470cdb0c
Linus <justwitti@gmail.com>
2020-08-01 12:15:32 +0200

    Revert "remove shop icons" (didn't work and freezes a lot)
    This reverts commit 04909ca79d161ea15d7dcf745fb58e6307c96017.

--- handlers/regex.go
+++ handlers/regex.go
@@ -4,24 +4,15 @@ import (
 	"regexp"
 )
 
-// Eliminate street names from packets
-// match [["Jl. SMA Aek Kota Batu","id"],["Sumatera Utara","de"]] (see regex_test.go)
-var streetNameChars = "(\\p{L}| |\\d|\\_|\\-|\\,|\\.|/)"
-var listOfStreetNames = "\\[\"" + streetNameChars + "+\"+,\"" + streetNameChars + "{1,10}\"\\]"
-var streetNameRegex *regexp.Regexp = regexp.MustCompile(listOfStreetNames)
+// GOAL : MATCH [["Jl. SMA Aek Kota Batu","id"],["Sumatera Utara","de"]]
+var stringRegex = "(\\p{L}| |\\d|\\_|\\-|\\,|\\.|/)"
+var languageRegex = "\\[\"" + stringRegex + "+\"+,\"" + stringRegex + "{1,10}\"\\]"
 
-// Eliminate shop icons from packets. These are an object in json. for an example, see regex_test.go.
-// Heuristic used: Always starts with [[[ and ends with ]]], not followed by a [\".
-// Always contains https://maps.gstatic.com/mapfiles/annotations/icons/ somewhere.
-// var shopChars = "(\\p{L}| |\\d|\\_|\\-|\\,|\\.|\\[|\\])"
-// var shopIcons = "[[[" + shopChars + "*?" + "https:\\/\\/maps.gstatic.com\\/mapfiles\\/annotations\\/icons\\/" + shopChars + "*?" + "]]],[^\\[\"]"
-var shopIcons = "\\[\\[\\[([\\[\\],\":\\/. \\\\_\\-\\pL\\pN\\pM\\pZ\\pP\\pC])*https:\\/\\/maps\\.gstatic\\.com\\/mapfiles\\/annotations\\/icons\\/([0-9\\[\\],\":\\/. \\\\_\\-\\pL])*\\]\\]\\],"
-var shopIconRegex *regexp.Regexp = regexp.MustCompile(shopIcons)
+var compiledRegexp *regexp.Regexp = regexp.MustCompile(languageRegex)
 
 // filterStrings filters all string contents from a given string (as byte array),
 // used to strip all localization information from a specific street view packet
 func filterStrings(body []byte) []byte {
-	result := streetNameRegex.ReplaceAllString(string(body), "[\"\",\"\"]")
-	result = shopIconRegex.ReplaceAllString(result, "[],")
+	result := compiledRegexp.ReplaceAllString(string(body), "[\"\",\"\"]")
 	return []byte(result)
 }
